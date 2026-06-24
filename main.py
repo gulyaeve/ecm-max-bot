f46f3804888b0336a02e4189fadf8f90d3d9a882
Eugene Gulyaev <gulyaeve@vk.com>
2026-06-24 14:35:15 +0300

    New ecm client

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from httpx import AsyncClient
 from maxapi import Bot, Dispatcher
 from maxapi.types import BotStarted
 
 from config import settings
 from logger import logger
-from utils.ecm import ecos_change_user_max_id
+from utils.ecm import ECMClient
 from utils.rc4_utils import decrypt_rc4
 
 
 bot = Bot(settings.MAX_BOT_TOKEN)
 dp = Dispatcher()
+http_client = AsyncClient()
+ecm_client = ECMClient(client=http_client)
 
 
 @dp.bot_started()
@@ -21,7 +24,7 @@ async def on_bot_started(event: BotStarted) -> None:
         try:
             decrypted_payload = decrypt_rc4(payload, settings.SECRET_KEY)
             user_id = decrypted_payload.split("_", 1)[1]
-            await ecos_change_user_max_id(max_id, user_id)
+            await ecm_client.ecos_change_user_max_id(max_id, user_id)
             await bot.send_message(
                 user_id=max_id,
                 text="Привет! Я сохранил твой MAX ID в ECM.",
