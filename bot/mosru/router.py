0a7287ba2c053e969d69419c6a141b3b31d2bab8
Eugene Gulyaev <gulyaeve@vk.com>
2026-06-25 10:51:58 +0300

    Added download file

--- bot/mosru/router.py
+++ bot/mosru/router.py
@@ -1,9 +1,10 @@
 from httpx import AsyncClient
 from maxapi.dispatcher import Router
 from maxapi import F
-from maxapi.types import MessageCreated
+from maxapi.types import InputMediaBuffer, MessageCreated
 from utils.ecm import ecm_client
 from config import settings
+from utils.http_utils import download_file_http
 from utils.redis import redis_client
 from logger import logger
 
@@ -21,24 +22,36 @@ async def save_token(event: MessageCreated):
             token_value_from_redis = await cache.get("mosru_token")
 
         logger.info(f"mosru token saved in redis {token_value_from_redis}")
-        # Тестовый запрос
-        async with AsyncClient() as client:
-            resp = await client.post(
-                url="https://prof.mos.ru/back/api/staff/search",
-                json={
-                    "page":0,
-                    "search":"",
-                    "size":100,
-                    "sort":["fullName,asc"]
-                },
-                headers={
-                    'Content-Type': 'application/json', # Говорим серверу, что хотим получить JSON
-                    'Authorization': f"Bearer {token_value_from_redis}", # Передаем токен авторизации
-                    "X-Mes-Subsystem": "proftechw_app"
-                }
-            )
-            
-        logger.info(f"mosru {resp.status_code}", exc_info=True, extra=resp.json())
-        await event.message.answer(
-            text=f"{resp.status_code}"
+
+        excel_file = await download_file_http(
+            url="https://prof.mos.ru/back/api/applications/report",
+            token=token_value_from_redis,
+            json={"learningYearId":1002678188,"rklCheckStatuses":[],"applicationPriority":[],"page":0,"size":10,"sort":["registrationDateTime,desc"]}
         )
+
+        media = InputMediaBuffer(buffer=excel_file, filename="data.xlsx")
+        await event.message.answer(
+            text="Вот твоя таблица:",
+            attachments=[media],
+        )
+        # Тестовый запрос
+        # async with AsyncClient() as client:
+        #     resp = await client.post(
+        #         url="https://prof.mos.ru/back/api/staff/search",
+        #         json={
+        #             "page":0,
+        #             "search":"",
+        #             "size":100,
+        #             "sort":["fullName,asc"]
+        #         },
+        #         headers={
+        #             'Content-Type': 'application/json', # Говорим серверу, что хотим получить JSON
+        #             'Authorization': f"Bearer {token_value_from_redis}", # Передаем токен авторизации
+        #             "X-Mes-Subsystem": "proftechw_app"
+        #         }
+        #     )
+            
+        # logger.info(f"mosru {resp.status_code}", exc_info=True, extra=resp.json())
+        # await event.message.answer(
+        #     text=f"{resp.status_code}"
+        # )
---
+++ utils/http_utils.py
@@ -0,0 +1,19 @@
+
+import io
+
+from httpx import AsyncClient
+
+
+async def download_file_http(url: str, token: str, json: dict, http_client: AsyncClient = AsyncClient()):
+    headers = {"Authorization": f"Bearer {token}"}
+    file_buffer = io.BytesIO()
+
+    async with http_client as session:
+        async with session.stream("GET", url, headers=headers, follow_redirects=True) as response:
+            response.raise_for_status()
+            
+            async for chunk in response.aiter_bytes():
+                file_buffer.write(chunk)
+
+    file_buffer.seek(0)
+    return file_buffer
