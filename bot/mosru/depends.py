3197c952de06b00be2fabd2101afdee2e8feb903
Eugene Gulyaev <gulyaeve@vk.com>
2026-06-26 09:42:43 +0300

    Added report priority

---
+++ bot/mosru/depends.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import io
+from pathlib import Path
+from typing import Optional
+import zipfile
+
+import anyio
+
+from utils.redis import redis_client
+from utils.http_utils import download_file_http
+from logger import logger
+from httpx import HTTPStatusError
+from config import settings
+from utils.max_bot import bot
+
+
+UPLOAD_DIR = Path("temp")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+
+async def create_reports_zip() -> Optional[str]:
+
+    async with redis_client as cache:
+        token_value_from_redis = await cache.get("mosru_token")
+
+    try:
+        final_zip_buffer = io.BytesIO()
+
+        with zipfile.ZipFile(
+            final_zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED
+        ) as main_zip:
+            for i in range(1, 5):
+                excel_file = await download_file_http(
+                    url="https://prof.mos.ru/back/api/applications/report",
+                    token=token_value_from_redis,
+                    json={
+                        "learningYearId": 1002678188,
+                        "rklCheckStatuses": [],
+                        "applicationPriority": [f"{str(i)}"],
+                        "page": 0,
+                        "size": 10,
+                        "sort": ["registrationDateTime,desc"],
+                    },
+                )
+
+                file_name_in_zip = f"report_{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}_priority_{i}.xlsx"
+                main_zip.writestr(file_name_in_zip, excel_file.read())
+
+        file_path = UPLOAD_DIR / f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.zip"
+
+        async with await anyio.open_file(file_path, "wb") as out_file:
+            await out_file.write(final_zip_buffer.getvalue())
+
+        final_zip_buffer.close()
+
+        return file_path
+
+    except HTTPStatusError as exc:
+        if exc.response.status_code == 401:
+            logger.warning("Ошибка: Невалидный Bearer токен!")
+
+            await bot.send_message(
+                user_id=settings.MAX_ID_TOKEN_HOLDER,
+                text="Добрый день! Обновите, пожалуйста, токен.\n\nС наилучшими пожеланиями, ИТ.Москва",
+            )
+
+        logger.warning("Ошибка внешнего сервера!")
--- bot/mosru/router.py
+++ bot/mosru/router.py
@@ -1,25 +1,14 @@
-from datetime import datetime
-from pathlib import Path
-
-from httpx import HTTPStatusError
 from maxapi.dispatcher import Router
 from maxapi import F
 from maxapi.types import Command, InputMedia, MessageCreated
+from bot.mosru.depends import create_reports_zip
 from utils.ecm import ecm_client
 from config import settings
-from utils.http_utils import download_file_http
 from utils.redis import redis_client
 from logger import logger
-from utils.max_bot import bot
 
 
 router = Router(router_id="mosru")
-
-
-UPLOAD_DIR = Path("temp")
-UPLOAD_DIR.mkdir(exist_ok=True)
-
-
 
 
 @router.message_created(F.message.body.text.startswith("token"))
@@ -47,44 +36,17 @@ async def save_token(event: MessageCreated):
 
 @router.message_created(Command("report"))
 async def send_file_with_reports(event: MessageCreated):
-    
+
     ecm_user_login = await ecm_client.get_user_login_from_ecm(event.from_user.user_id)
 
     if ecm_user_login in settings.ECM_LOGINS_FOR_REPORT:
-        async with redis_client as cache:
-            token_value_from_redis = await cache.get("mosru_token")
         try:
-            excel_file = await download_file_http(
-                url="https://prof.mos.ru/back/api/applications/report",
-                token=token_value_from_redis,
-                json={"learningYearId":1002678188,"rklCheckStatuses":[],"applicationPriority":[],"page":0,"size":10,"sort":["registrationDateTime,desc"]}
+            file_path = await create_reports_zip()
+
+            media = InputMedia(file_path)
+            await event.message.answer(
+                text="Отчёты по приоритетам", attachments=[media]
             )
-        except HTTPStatusError as exc:
-            if exc.response.status_code == 401:
-                logger.warning("Ошибка: Невалидный Bearer токен!")
-                
-                await bot.send_message(
-                    user_id=settings.MAX_ID_TOKEN_HOLDER,
-                    text="Добрый день! Обновите, пожалуйста, токен.\n\nС наилучшими пожеланиями, ИТ.Москва"
-                )
-                
-            logger.warning("Ошибка внешнего сервера!")
-
-        file_path = UPLOAD_DIR / f"{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.xlsx"
-
-        with open(file_path, "wb") as f:
-            f.write(excel_file.getvalue())
-
-        media = InputMedia(file_path)
-        await event.message.answer(
-            text="Отчёт",
-            attachments=[media]
-        )
-                
-
-        # file_path = UPLOAD_DIR / f"{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.xlsx"
-
-        
-        # await event.message.answer(
-        #     text=f"Я сохранил таблицу в {file_path}",
-        # )
+        except Exception as e:
+            logger.warning(f"Report create error {e}")
+            await event.message.answer("Произошла ошибка при формировании отчёта")
